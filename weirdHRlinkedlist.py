c615638596dfd7ee377fc73288a2005dd2d53a60
Tin Le <tlv144199@gmail.com>
2020-02-24 22:15:12 -0600

    Update weirdHRlinkedlist.py

--- weirdHRlinkedlist.py
+++ weirdHRlinkedlist.py
@@ -1,3 +1,5 @@
+#Linked list insert at the end implementation for Hacker Rank
+#problem
 class Node:
     def __init__(self,data):
         self.data = data
@@ -9,6 +11,9 @@ class Solution:
             print(current.data,end=' ')
             current = current.next
     # insertion method for the linked list
+    #This method uses recursion and checks for three\
+    #possible cases: empty linked list single element
+    # and finally a full linked list
     def insert(self,head,data):
         if (head == None):
             head = Node(data)
